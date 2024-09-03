27261d6ac4a6d0fb380ee4149221f545a8bad787
Abhineetsahay <abhineetsahay@gmail.com>
2024-09-03 19:25:59 +0530

    Add Log out

--- src/Pages/DashbBoard.tsx
+++ src/Pages/DashbBoard.tsx
@@ -1,7 +1,25 @@
+import { Button } from "@mui/material";
+import { auth } from "../components/Firebase/Firebase";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
 const DashBoard=()=>{
+          const navigate=useNavigate();
+          const LogOut=async()=>{
+                    try {
+                              await auth.signOut();
+                              toast.success("User Logged Successfully")
+                              navigate("/auth")
+                    } catch (error) {
+                              toast.error("Error");
+                    }
+          }
           return (
                     <>
-                    In Profile
+                    {/* In Profile */}
+                    <div>
+                    <Button variant="contained" color="primary" onClick={LogOut}>Log Out</Button>
+                    </div>
                     </>
           )
 }
